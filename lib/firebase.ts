70bea77b028e9042eb8656912318c20fde6e80ed
hurihara <fai1023wasi@gmail.com>
2026-06-18 08:07:05 +0900

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAeXvxoEIUl7BFb9YCt5YzVjjFtixQmukk",
@@ -14,3 +15,21 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+export const messaging = typeof window !== 'undefined' ? getMessaging(app) : null;
+
+export const requestNotificationPermission = async () => {
+  if (!messaging) return null
+  try {
+    const permission = await Notification.requestPermission()
+    if (permission === 'granted') {
+      const token = await getToken(messaging, {
+        vapidKey: 'ここに鍵を貼り付けて'
+      })
+      return token
+    }
+  } catch (e) {
+    console.error(e)
+  }
+  return null
+}
