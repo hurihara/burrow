49bfeba7d330e65decc67448d7824057418ed6c7
hurihara <fai1023wasi@gmail.com>
2026-06-17 18:39:45 +0900

    Create firebase.ts

---
+++ lib/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAeXvxoEIUl7BFb9YCt5YzVjjFtixQmukk",
+  authDomain: "burrow-90f6b.firebaseapp.com",
+  projectId: "burrow-90f6b",
+  storageBucket: "burrow-90f6b.firebasestorage.app",
+  messagingSenderId: "608885449270",
+  appId: "1:608885449270:web:cb31781a9180d518afae97"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
